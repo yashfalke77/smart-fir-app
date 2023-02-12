0d87d7624007880ac0fab90fcdbd1d5f21f02224
Siddhesh2811 <wani.siddhesh.siddhesh2@gmail.com>
2023-02-12 21:42:58 +0530

    Footer Updates

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,35 +1,42 @@
-import React from 'react'
+import React from "react";
 import "./footer.scss";
+import logo from "../../assets/images/logo.png";
 
 const Footer = () => {
-
     const contacts = [
         "Traffic WhatsApp Helpline : 8454999999",
         "Citizen Wall : 103",
         "Control Room : 100",
-        "Elder Line : 1090"
-    ]
+        "Elder Line : 1090",
+    ];
 
     return (
         <footer>
-            <footer className='footer'>
-                <hr />
-                <div className="contacts">
-                    <ul className='contacts__list'>
-                        {contacts.map((contact, index) => <li key={index}>{contact}</li>)}
-                    </ul>
+            <footer className="footer">
+                <div className="footer__left">
+                    <div className="contacts">
+                        <ul className="contacts__list">
+                            {contacts.map((contact, index) => (
+                                <li className="contacts__items" key={index}>{contact}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="copyright">
+                        <ul className="copyright__list">
+                            <li className="copyright__item">
+                                Copyright © 2022 Mumbai Police | Developed By : Dreamcare
+                                Developers
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <hr />
-
-                <div className='copyright'>
-                    <ul className='copyright__list'>
-                        <li className='copyright__item'>Copyright © 2022 Mumbai Police | Developed By : Dreamcare Developers</li>
-                        <li className='copyright__item'>Last Update On: 02-Feb-2023 23:20:57 </li>
-                    </ul>
+                <div className="footer__right">
+                    <img className="footer__image" src= {logo} alt="" />
+                    <h2>Cyber Police</h2>
                 </div>
             </footer>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
