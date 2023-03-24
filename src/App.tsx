4d529b7bdaa6f5454deed3ebb811849cb3531fe8
Yash Falke <yashfalke77@gmail.com>
2023-03-24 23:01:19 +0530

    enforcing coding style

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import './App.scss'
+import React from 'react';
+import './App.scss';
 
 function App() {
-
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-
+        Hello world!
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
